a8514abc0345375ec2908d9268814d9ffabe9273
gowhale <gabewhale@gmail.com>
2020-08-23 20:25:07 +0100

    RED - Now expecting FIZZBUZZ when 15 is passed

--- fizz_buzz_test.go
+++ fizz_buzz_test.go
@@ -13,3 +13,7 @@ func TestThreeIsFizz(t *testing.T) {
 func TestFiveIsBuzz(t *testing.T) {
 	assert.EqualValues(t, FizzBuzz(5), "BUZZ")
 }
+
+func TestFifteenIsFizzBuzz(t *testing.T) {
+	assert.EqualValues(t, FizzBuzz(15), "FIZZBUZZ")
+}
